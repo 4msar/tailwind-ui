4827ceb36725bf1e5ace97617d352d0fc2fe58bf
Saiful Alam Rakib <saiful.rakib@miros-group.com>
2025-02-11 11:25:07 +0600

    Added few components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ArrowDown } from 'lucide-vue-next';
+import CurvedImage from './components/CurvedImage.vue'
+import StickyTitle from './components/StickyTitle.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="flex flex-col gap-y-8 divide-y divide-slate-300">
+    <header class="h-screen w-full flex flex-col border-b border-slate-300 justify-center items-center">
+      <h1 class="container mx-auto text-dynamic p-4 font-bold mb-8 text-center text-slate-700">
+        Tailwind UIs with Vite + Vue 3 + TypeScript
+      </h1>
+      <ArrowDown class="w-12 h-12 mt-8 text-slate-700 animate-bounce" />
+    </header>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <!-- Main Component Lists -->
+    <section class="h-screen">
+      <CurvedImage class="container mx-auto" />
+    </section>
+    <section class="h-screen">
+      <StickyTitle class="container mx-auto" />
+    </section>
+  </main>
+
+
+</template>
---
+++ src/components/CurvedImage.vue
@@ -0,0 +1,72 @@
+<template>
+    <section>
+        <div class="rellax mb-96 overflow-hidden px-1" style="transform: translate3d(0px, 110px, 0px);">
+            <div class="flex items-center justify-center gap-1">
+                <div class="h-96 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-one.webp"
+                        alt="lifestyle" width="1380" height="1380">
+                </div>
+                <div class="h-80 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-two.webp"
+                        alt="lifestyle" width="1380" height="1380">
+                </div>
+                <div class="h-72 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-three.webp"
+                        alt="lifestyle" width="826" height="1239">
+                </div>
+                <div class="h-64 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-four.webp"
+                        alt="lifestyle" width="826" height="1239">
+                </div>
+                <div
+                    class="hidden h-60 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-five.webp"
+                        alt="lifestyle" width="826" height="1239">
+                </div>
+                <div
+                    class="hidden h-56 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-six.webp"
+                        alt="lifestyle" width="2000" height="1335">
+                </div>
+                <div
+                    class="hidden h-52 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-seven.webp"
+                        alt="lifestyle" width="900" height="1261">
+                </div>
+                <div
+                    class="hidden h-52 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-eight.webp"
+                        alt="lifestyle" width="2000" height="1330">
+                </div>
+                <div
+                    class="hidden h-56 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-nine.webp"
+                        alt="lifestyle" width="996" height="1245">
+                </div>
+                <div
+                    class="hidden h-60 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96 sm:block">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-ten.webp"
+                        alt="lifestyle" width="2000" height="1333">
+                </div>
+                <div class="h-64 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-eleven.webp"
+                        alt="lifestyle" width="826" height="1239">
+                </div>
+                <div class="h-72 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-twelve.webp"
+                        alt="lifestyle" width="1380" height="1380">
+                </div>
+                <div class="h-80 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover"
+                        src="https://tlfolio.netlify.app/images/me/show-thirteen.webp" alt="lifestyle" width="1380"
+                        height="1380">
+                </div>
+                <div class="h-96 w-20 overflow-hidden rounded-md transition-all duration-500 hover:w-96">
+                    <img class="h-full w-full object-cover"
+                        src="https://tlfolio.netlify.app/images/me/show-fourteen.webp" alt="lifestyle" width="740"
+                        height="1110">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
